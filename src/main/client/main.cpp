d4225f48b8bc4f17cc2ad369f39c7e619bd55460
Agustina Barbetta <agustina.barbetta@gmail.com>
2018-05-31 01:43:36 -0300

    Client main

--- src/main/client/main.cpp
+++ src/main/client/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
-int main() {
+#include "Client.h"
+
+int main(int argc, char* argv[]) {
     std::cout << "Hello, World! I'm the client" << std::endl;
+
+    Client c(std::stoi(argv[1]));
+    c.run();
+
     return 0;
 }
