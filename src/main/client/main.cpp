7539e2d74803b6b0c17247b1f8ea02f47b6f0af5
Manuel Porto <manu.porto94@gmail.com>
2018-06-18 10:56:29 -0300

    Define parameters on definitions file

--- src/main/client/main.cpp
+++ src/main/client/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
 #include "Client.h"
+#include "FileNames.h"
 
 int main(int argc, char* argv[]) {
     std::cout << "Hello, World! I'm the client" << std::endl;
 
-    Client c(std::string(argv[1]), argv[2][0]);
+    Client c(MessageQueueInfo::FILE, MessageQueueInfo::LETTER);
     c.run();
 
     return 0;
--- src/main/server/main.cpp
+++ src/main/server/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+
+#include "FileNames.h"
 #include "Server.h"
 
 int main(int argc, char* argv[]) {
     std::cout << "Hello, World! I'm the server" << std::endl;
 
-    Server s(std::string(argv[1]), argv[2][0], std::string(argv[3]));
+    Server s(MessageQueueInfo::FILE, MessageQueueInfo::LETTER, DatabaseInfo::DB_FILE);
     s.run();
 
     return 0;
