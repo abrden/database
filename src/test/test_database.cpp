b63159e7b6cb62f952f8f142d7658ba97a19be69
Manuel Porto <manu.porto94@gmail.com>
2018-06-22 16:09:00 -0300

    Add tests

--- src/test/test_database.cpp
+++ src/test/test_database.cpp
@@ -6,16 +6,46 @@ TEST_CASE("Database", "[db]") {
 
     Entry entry("Juan Perez", "Calle Falsa 123", "911");
 
-    SECTION("entry_exists()") {
+    SECTION("entry_exists() with empty db") {
         Database db;
 
         REQUIRE_FALSE(db.entry_exists(&entry));
     }
 
-    SECTION("insert_entry()") {
+    SECTION("entry_exists() with entry in the db") {
         Database db;
         db.insert_entry(&entry);
 
         REQUIRE(db.entry_exists(&entry));
     }
+
+    SECTION("insert_entry() with non-duplicate entry") {
+        Database db;
+
+        REQUIRE_NOTHROW(db.insert_entry(&entry));
+    }
+
+    SECTION("insert_entry() with duplicate entry") {
+        Database db;
+        db.insert_entry(&entry);
+
+        REQUIRE_THROWS_AS(db.insert_entry(&entry), std::runtime_error);
+    }
+
+    SECTION("select_entries() with non-existent entry") {
+        Database db;
+
+        std::vector<Entry*> entries = db.select_entries(&entry);
+
+        REQUIRE(entries.empty());
+    }
+
+//    SECTION("select_entries() with existent entry") {
+//        Database db;
+//        db.insert_entry(&entry);
+//
+//        std::vector entries = db.select_entries(&entry);
+//
+//        REQUIRE_FALSE(entries.empty());
+//    }
 }
